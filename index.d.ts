420ce23e040f5deab0ee1a940c1960e18d9a0ae6
Preport <erdemgezzz@gmail.com>
2021-05-10 16:28:19 +0300

    Add missing functions

--- index.d.ts
+++ index.d.ts
@@ -8,4 +8,14 @@ export default class BptfLogin {
     getAPIKey(callback: (err?: Error, apiKey?: string) => void): void;
 
     generateAPIKey(domain: string, description: string, callback: (err?: Error, apiKey?: string) => void): void;
+
+    revokeAPIKey(apiKey: string, callback: (err?: Error) => void): void;
+
+    getSettings(callback: (err?: Error, settings?: { [setting: string]: string }) => void): void;
+
+    updateSettings(settings: { [setting: string]: string }, callback: (err?: Error, settings?: { [setting: string]: string }) => void): void;
+
+    generateAccessToken(callback: (err?: Error, accessToken?: string) => void): void;
+
+    _getUserID(): void;
 }
