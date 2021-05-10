9dc8903c097843219e5a58c5428ce654e839e3da
Preport <erdemgezzz@gmail.com>
2021-05-10 16:21:17 +0300

    Fix Type Declaration

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,11 @@
-declare module 'bptf-login-2' {
-    class BptfLogin {
-        setCookies(cookies: string[]): void;
+export default class BptfLogin {
+    setCookies(cookies: string[]): void;
 
-        login(callback: (err?: Error) => void): void;
+    login(callback: (err?: Error) => void): void;
 
-        getAccessToken(callback: (err?: Error, accessToken?: string) => void): void;
+    getAccessToken(callback: (err?: Error, accessToken?: string) => void): void;
 
-        getAPIKey(callback: (err?: Error, apiKey?: string) => void): void;
+    getAPIKey(callback: (err?: Error, apiKey?: string) => void): void;
 
-        generateAPIKey(domain: string, description: string, callback: (err?: Error, apiKey?: string) => void): void;
-    }
-
-    export = BptfLogin;
+    generateAPIKey(domain: string, description: string, callback: (err?: Error, apiKey?: string) => void): void;
 }
